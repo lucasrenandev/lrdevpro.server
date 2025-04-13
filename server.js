5ceddd8bde0e2f861c2115cd56ff6343b6118e69
lucasrenandev <lucasrenandns96@gmail.com>
2025-04-13 11:03:48 -0300

    Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
+//const path = require("path");
 const nodemailer = require("nodemailer");
 
 const app = express();
